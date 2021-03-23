5b3aa45bc8dc1fa0c5cba2a6db326e5d7969f184
84snel <84snel@gmail.com>
2021-03-24 00:42:12 +0200

    fixes_&_adds_access_from_the_countries_list

--- src/index.js
+++ src/index.js
@@ -3,16 +3,10 @@ import './js/theme-switch';
 import { notific } from './js/notific';
 
 import fetchCountries from './js/fetchCountries';
-const debounce = require('lodash.debounce');
+import debounce from 'lodash.debounce';
 
-import countryInfo from './templates/countryInfo.hbs';
-import countriesList from './templates/countriesList.hbs';
-
-// const markup = menuTemplate(menuData);
-
-// const menuRef = document.querySelector('ul.js-menu');
-
-// menuRef.insertAdjacentHTML('beforeend', markup);
+import countryInfoTpl from './templates/countryInfo.hbs';
+import countriesListTpl from './templates/countriesList.hbs';
 
 const countriesWrapperRef = document.querySelector('.countries__wrapper');
 const inputRef = document.getElementById('country-input');
@@ -20,8 +14,16 @@ const inputRef = document.getElementById('country-input');
 inputRef.addEventListener('input', debounce(handleQuery, 500));
 
 function handleQuery({ target: { value } }) {
-  fetchCountries(value)
+  if (!value.trim()) {
+    clearPage();
+    return notific.myAlert('Start typing the country name');
+  }
+  fetchCountries(value.trim())
     .then(countriesList => {
+      if (countriesList.status === 404) {
+        clearPage();
+        return notific.myError('country not found. Re-enter');
+      }
       renderePage(countriesList);
     })
     .catch(err => {
@@ -30,28 +32,37 @@ function handleQuery({ target: { value } }) {
 }
 
 function renderePage(countries) {
-  countriesWrapperRef.innerHTML = '';
+  clearPage();
   if (countries.length > 10) {
     return notific.myAlert(
       'Too many matches found. PLease enter a more specific query!',
     );
   }
 
-  if (countries.length === 10)
-    return notific.myError(
-      'Too many matches found. PLease enter a more specific query!',
-    );
-
   if (countries.length === 1) {
     countriesWrapperRef.insertAdjacentHTML(
       'beforeend',
-      countryInfo(...countries),
+      countryInfoTpl(...countries),
     );
   }
+
   if (countries.length > 1 && countries.length < 10) {
     countriesWrapperRef.insertAdjacentHTML(
       'beforeend',
-      countriesList(countries),
+      countriesListTpl(countries),
     );
+    addListener(countries);
+  }
+}
+
+function clearPage() {
+  countriesWrapperRef.innerHTML = '';
+}
+function addListener(countries) {
+  countriesWrapperRef.addEventListener('click', onClick);
+  function onClick({ target: { innerText } }) {
+    const count = countries.findIndex(c => c.name === innerText);
+    countriesWrapperRef.removeEventListener('click', onClick);
+    renderePage([countries[count]]);
   }
 }
